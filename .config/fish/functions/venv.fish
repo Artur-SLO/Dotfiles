93d1a9f5bed705a1482878e84147ec3750ef0cce
SlowerTrick <arturvitorcintra@gmail.com>
2026-05-04 20:58:40 -0300

    main browser switched to Zen

---
+++ .config/fish/functions/venv.fish
@@ -0,0 +1,3 @@
+function venv --wraps='python -m venv .venv && source .venv/bin/activate.fish' --description 'alias venv=python -m venv .venv && source .venv/bin/activate.fish'
+    python -m venv .venv && source .venv/bin/activate.fish $argv
+end
